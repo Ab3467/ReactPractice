331bfeb78e4130203e0438d2f61d773a95f94dca
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-23 18:09:23 +0500

    changes

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Don't change the Component name "App"
+export default function App() {
+    const [isActive, setIsActive] = useState(false);
+
+    const toggleStyle = () => {
+        setIsActive(true);
+    };
+
+    return (
+        <div>
+            <p className={isActive ? 'active' : ''}>Style me!</p>
+            <button onClick={toggleStyle}>Toggle style</button>
+        </div>
+    );
 }
-
-export default App;
