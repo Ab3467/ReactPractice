6228a0a6077abf750f595b8daf20b5f02f4a1de0
Ahmad abu bakr <ahmed.nums345@gmail.com>
2024-04-26 17:31:06 +0500

    Changes

--- my-app/src/components/CoreConcepts.js
+++ my-app/src/components/CoreConcepts.js
@@ -1,8 +1,9 @@
 import React from 'react'
+import "./index.css"
 
 export default function CoreConcepts(props) {
   return (
-    <div>
+    <div className="CoreConcepts">
       
         <h3>{props.title}</h3>
         <p>{props.description}</p>
